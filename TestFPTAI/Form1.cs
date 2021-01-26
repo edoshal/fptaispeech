0fcb1b6b7d8cc37d2f612b591998a6beaedfa4c8
Len Hau <mrhau.qs@gmail.com>
2021-01-26 20:42:21 +0700

    commit source

---
+++ TestFPTAI/Form1.cs
@@ -0,0 +1,73 @@
+﻿using NAudio.Wave;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestFPTAI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String result = Task.Run(async () =>
+            {
+                String payload = "Bông Hôi láo quá, cẩn thận bị mẹ đánh";
+                HttpClient client = new HttpClient();
+                client.DefaultRequestHeaders.Add("api-key", "");
+                client.DefaultRequestHeaders.Add("speed", "");
+                client.DefaultRequestHeaders.Add("voice", "thuminh");
+                var response = await client.PostAsync("https://api.fpt.ai/hmi/tts/v5", new StringContent(payload));
+                return await response.Content.ReadAsStringAsync();
+            }).GetAwaiter().GetResult();
+
+            Console.WriteLine(result);
+
+            var obj = JsonConvert.DeserializeObject<SpeechText>(result);
+
+            PlayMp3FromUrl(obj.async);
+        }
+
+        public void PlayMp3FromUrl(string url)
+        {
+            using (Stream ms = new MemoryStream())
+            {
+                using (Stream stream = WebRequest.Create(url)
+                    .GetResponse().GetResponseStream())
+                {
+                    byte[] buffer = new byte[32768];
+                    int read;
+                    while ((read = stream.Read(buffer, 0, buffer.Length)) > 0)
+                    {
+                        ms.Write(buffer, 0, read);
+                    }
+                }
+
+                ms.Position = 0;
+                using (WaveStream blockAlignedStream =
+                    new BlockAlignReductionStream(
+                        WaveFormatConversionStream.CreatePcmStream(
+                            new Mp3FileReader(ms))))
+                {
+                    using (WaveOut waveOut = new WaveOut(WaveCallbackInfo.FunctionCallback()))
+                    {
+                        waveOut.Init(blockAlignedStream);
+                        waveOut.Play();
+                        while (waveOut.PlaybackState == PlaybackState.Playing)
+                        {
+                            System.Threading.Thread.Sleep(100);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ TestFPTAI/SpeechText.cs
@@ -0,0 +1,10 @@
+﻿namespace TestFPTAI
+{
+    class SpeechText
+    {
+        public string async { get; set; }
+        public int error { get; set; }
+        public string message { get; set; }
+        public string request_id { get; set; }
+    }
+}
